ceb03c3e1afa3d24da13918cd21884ce0b2b262f
Gongas <goncalo.mesteves03@gmail.com>
2025-03-24 19:02:45 +0000

    Implemented some more functions to help analyse the data

--- Machine-Learning-Pipeline/DataPreprocessing/DatasetManager.py
+++ Machine-Learning-Pipeline/DataPreprocessing/DatasetManager.py
@@ -2,8 +2,9 @@ from typing import (Tuple)
 import numpy as np
 import pandas as pd
 import os
-from pyspark.sql import SparkSession
+from pyspark.sql import (SparkSession)
 from pyspark.sql.dataframe import (DataFrame)
+from pyspark.sql.functions import (col, isnan, count, when, countDistinct, lit, round as pysparkRound, desc)
 
 # Custom Imports
 from .CustomDecorators import (timeit, resourceProfiler)
@@ -41,6 +42,14 @@ class DatasetManager:
         # Save a variable for the main dataframe to use
         self.df = None
 
+    # @timeit
+    @resourceProfiler
+    def testingDecoratorsFunction(self):
+        # Testing the decorator
+        # print(self.config)
+        l = [i for i in range(100_000)]
+        return np.max(l)
+
     @staticmethod
     def shape(dataframe:DataFrame) -> Tuple[int, int]:
         """
@@ -59,37 +68,126 @@ class DatasetManager:
         # Return the number of rows and columns on the dataframe
         return (dataframe.count(), len(dataframe.columns))
 
-    # @timeit
-    @resourceProfiler
-    def printConfig(self):
-        # Testing the decorator
-        # print(self.config)
-        l = [i for i in range(100_000)]
-        return np.max(l)
-
     @staticmethod
-    def checkPath(path:str) -> bool:
+    def getColumnMissingValues(df:DataFrame, column:str) -> DataFrame:
         """
         # Description
-            -> This static method allows to make sure that the nested
-            directories of a given path exist and if not create them.
-        -------------------------------------------------------------
+            -> This method aims to showcase the amount of missing
+            values of a given column on a certain DataFrame.
+        ---------------------------------------------------------
         # Params:
         ---------
-            - path : str -> A arbitrary path.
+            - df: pyspark DataFrame -> DataFrame to consider for the operation.
+            - column: str -> Column of the dataframe to evaluate the unique values.
+        ---------
+        # Returns
+        ---------
+            - Pyspark DataFrame with the amount of missing values on the given column of the DataFrame.
+        """
+        
+        # Select and count the amount of missing values on the given column/feature
+        missingValues = df.select(
+            count(when(col(column).isNull() | isnan(column), column)).alias(f"[{column}] Missing Values")
+        )
+
+        # Return the missing values dataframe with the amount of missing data
+        return missingValues
+    
+    @staticmethod
+    def checkMissingValues(df:DataFrame) -> None:
+        """
+        # Description
+            -> This method aims to compute the amount of missing
+            values on each one of the dataframe's columns.
+        --------------------------------------------------------
+        # Params:
+        ---------
+            - df: pyspark DataFrame -> DataFrame to consider for the operation.
         ----------
         # Returns:
         ----------
-            - A boolean value that returns if a file exists and if not creates the directory to accomudate it.
+            - None, since we are only showcasing the amount of missing values per column.
+        """
+    
+        # Compute the total number of rows
+        totalCount = df.count()
+
+        # Get the missing values for each one of the dataframe's columns
+        missingValues = df.select(
+            [count(when(col(c).isNull(), 1)).alias(c) for c in df.columns]
+        )
+
+        # Pivot the one-row DataFrame into two columns
+        n = len(df.columns)
+        stackExpressions = "stack({0}, {1}) as (Column, MissingValuesCount)".format(
+            n,
+            ", ".join([f"'{c}', {c}" for c in df.columns])
+        )
+
+        # Create the vertical DataFrame
+        missingValues = missingValues.selectExpr(stackExpressions)
+
+        # Add the percentage column and sort descrescently based on it
+        missingValues = missingValues.withColumn(
+            "MissingValuesPercentage(%)",
+            pysparkRound((col("MissingValuesCount") / lit(totalCount)) * 100, 2)
+        ).orderBy(desc("MissingValuesPercentage(%)"))
+
+        # Display the missing values
+        missingValues.show()
+
+    @staticmethod
+    def getColumnUniqueValues(df:DataFrame, column:str) -> DataFrame:
+        """
+        # Description
+            -> This method aims to showcase all the unique
+            values of a given column on a certain DataFrame.
+        ----------------------------------------------------
+        # Params:
+        ---------
+            - df: pyspark DataFrame -> DataFrame to consider for the operation.
+            - column: str -> Column of the dataframe to evaluate the unique values.
+        ---------
+        # Returns
+        ---------
+            - Pysaprk DataFrame with the unique values of a given column.
         """
         
-        # Checks if the file exists
-        if os.path.exists(path):
-            return True
-        else:
-            # Make sure that the nested directories exist
-            os.makedirs(os.path.dirname(path), exist_ok=True)
-            return False
+        # Select and get the unique values of the given column
+        uniqueValues = (
+            df.groupBy(column)\
+            .count()\
+            .withColumnRenamed("count", "Total Count")\
+            .orderBy(column)
+        )
+
+        # Return the unique values of the given column
+        return uniqueValues
+    
+    @staticmethod
+    def checkColumnsAmountUniqueValues(df:DataFrame) -> None:
+        """
+        # Description
+            -> This method aims to compute the amount of unique
+            values on each one of the dataframe's columns.
+        --------------------------------------------------------
+        # Params:
+        ---------
+            - df: pyspark DataFrame -> DataFrame to consider for the operation.
+        ----------
+        # Returns:
+        ----------
+            - None, since we are only showcasing the amount of missing values per column.
+        """
+
+        # Create expressions for counting distinct values in each column
+        countDistinctExpressions = [countDistinct(col(c)).alias(c) for c in df.columns]
+
+        # Aggregate those counts into one row
+        amountUniqueValues = df.agg(*countDistinctExpressions)
+
+        # Showcase the data
+        amountUniqueValues.show(vertical=True)
         
     def loadDataFrame(self, filename:str) -> DataFrame:
         """
